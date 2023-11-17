7b48d1f1c211dbb0393fc69cb74e22ebce1776e9
Martim Félix <a100647@alunos.uminho.pt>
2023-11-17 11:29:53 +0000

    Completei o ficheiro h do parsing que não estava a incluir todas as funções

--- trabalho-pratico/src/parsing.h
+++ trabalho-pratico/src/parsing.h
@@ -20,5 +20,10 @@ int valid_price(char *string);
 int valid_rating(char *string);
 int compare_begin_with_end(char *string1, char *string2);
 int is_non_empty_string(char *string);
+void parse_users_csv(hash_user h, char *line);
+void parse_reservations_csv(hash_user h, char *line);
+void parse_passengers_csv(hash_user h, char *line);
+void parse_flights_csv(hash_user h, char *line);
+void process_csv_file(const char* filename, hash_user h, void (*parse_function)(hash_user, char*));
 
 #endif  // PARSING_H
