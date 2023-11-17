d36eb7408f122c8e790e29fb6654f17aef2b83c6
Martim Félix <a100647@alunos.uminho.pt>
2023-11-17 11:26:26 +0000

    Completei o include dos users

--- trabalho-pratico/src/user.h
+++ trabalho-pratico/src/user.h
@@ -60,3 +60,22 @@ int Hash(char *id);
 // Função para inicializar a tabela hash
 void InitializeTable(hash_user h);
 
+//Função para inserir o voo na tabela hash
+void InsertPassenger(hash_user h, char *id,char *voo_id); 
+
+
+//Função para inserir a reserva na tabela hash 
+void InsertReserva(hash_user h, char *id, char *reserva_id, char *hotel_id, char *hotel_name, char *hotel_stars, char *city_tax, char *address, char *begin_date, char *end_date, char *price_per_night, char *includes_breakfast, char *room_details, char* rating, char *comment); 
+
+
+// Função para inserir na tabela hash
+void InsertTable(hash_user h, char *id, char *nome, char *email, char *phone_number, char *birth_date, char *sex, char *passport, char* country_code, char *address, char* account_creation, char *pay_method, char *account_status);
+
+//Função para inserir na tabela hash o schedule_departure_date
+void InsertScheduleDepartureDateByVooId(hash_user h, char *voo_id, char *schedule_departure_date);
+
+// Função para imprimir a tabela hash
+void Printhash_user(hash_user h);
+
+//Função que vai buscar um User á HashTable através do seu ID
+User *RetrieveUser(hash_user h, char *id);
