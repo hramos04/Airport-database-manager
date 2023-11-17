abf477f66338ebede2e0bc72c3d49a865655ada0
joaoloureiro17 <a100832@alunos.uminho.pt>
2023-11-17 15:14:16 +0000

    já mete a lista ligada de reservas

--- trabalho-pratico/src/main.c
+++ trabalho-pratico/src/main.c
@@ -4,21 +4,6 @@
 #include "user.h"
 #include "parsing.h"
 
-
-int CountElements(hash_user h) {
-    int count = 0;
-
-    for (int i = 0; i < HASHSIZE; ++i) {
-        User *current = h[i];
-        while (current != NULL) {
-            count++;
-            current = current->next;
-        }
-    }
-
-    return count;
-}
-
 int main(int argc, char *argv[]) {
 	
     hash_user minhaTabela;
@@ -29,29 +14,26 @@ int main(int argc, char *argv[]) {
 	strcpy(csv_users, argv[1]);
 	strcat(csv_users, "/users.csv");
 	
-	//char *csv_reservas = (char*)malloc(256);
-	//strcpy(csv_reservas, argv[1]);
-	//strcat(csv_reservas, "/reservations.csv");
+	char *csv_reservas = (char*)malloc(256);
+	strcpy(csv_reservas, argv[1]);
+	strcat(csv_reservas, "/reservations.csv");
 	
-	//char *csv_passageiros = (char*)malloc(256);
-	//strcpy(csv_passageiros, argv[1]);
-	//strcat(csv_passageiros, "/passengers.csv");
+	char *csv_passageiros = (char*)malloc(256);
+	strcpy(csv_passageiros, argv[1]);
+	strcat(csv_passageiros, "/passengers.csv");
 
     //char *csv_voos = (char*)malloc(256);
 	//strcpy(csv_voos, argv[1]);
 	//strcat(csv_voos, "/flights.csv");
 
     process_csv_file(csv_users, minhaTabela, parse_users_csv);
-    //process_csv_file(csv_reservas, minhaTabela, parse_reservations_csv);
+    process_csv_file(csv_reservas, minhaTabela, parse_reservations_csv);
     //process_csv_file(csv_passageiros, minhaTabela, parse_passengers_csv);
     //process_csv_file(csv_voos, minhaTabela, parse_flights_csv);
-
-    
 
     // Imprimir conteúdo da tabela hash
     printf("Conteúdo da Tabela Hash:\n");
     Printhash_user(minhaTabela);
-    printf("Número de elementos na tabela hash: %d\n", CountElements(minhaTabela));
 
     return 0;
 }
