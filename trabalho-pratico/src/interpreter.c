45591b148c800ff93825fabe9f92fc8242ade4e0
joaoloureiro17 <a100832@alunos.uminho.pt>
2023-11-19 18:21:11 +0000

    correção do erro na querie 5

--- trabalho-pratico/src/interpreter.c
+++ trabalho-pratico/src/interpreter.c
@@ -282,7 +282,8 @@ void q5(hash_aeroportos h_aeroportos, char *origin, char *begin_date, char *end_
 			aux = aux->next_resumo;
 		}
 	}
-	
+}
+
 int comando(char *linha, hash_user h_users, hash_voos h_voos, hash_reservas h_reservas, hash_hoteis h_hoteis, hash_aeroportos h_aeroportos, FILE *fp_output) {
 	int argc = 0;
 	char *args[MAX_ARGS];
