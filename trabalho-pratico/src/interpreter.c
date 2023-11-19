f2bc21426c08fa432d0ef95618042150324293cb
Martim Félix <a100647@alunos.uminho.pt>
2023-11-19 18:10:47 +0000

    correção Q5

--- trabalho-pratico/src/interpreter.c
+++ trabalho-pratico/src/interpreter.c
@@ -282,6 +282,7 @@ void q5(hash_aeroportos h_aeroportos, char *origin, char *begin_date, char *end_
 			aux = aux->next_resumo;
 		}
 	}
+}
 	
 int comando(char *linha, hash_user h_users, hash_voos h_voos, hash_reservas h_reservas, hash_hoteis h_hoteis, hash_aeroportos h_aeroportos, FILE *fp_output) {
 	int argc = 0;
